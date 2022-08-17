4c9a5d9dc23d8b61b78e12c1cedf0b6c3c91228d
bangng <montersb@gmail.com>
2022-08-17 21:38:09 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+        copyFile("D:\\IdeaProjects\\MD2_B17_BT2_CopyBinary\\src\\data.txt","D:\\IdeaProjects\\MD2_B17_BT2_CopyBinary\\src\\copy-data.txt");
+    }
+    public static void copyFile(String src, String dest){
+        File in = new File(src);
+        File out = new File(dest);
+        if (!in.exists()){
+            System.out.println("Source file does not exist");
+            return;
+        }
+        if (out.exists()){
+            System.out.println("Destination file already exists and will be overwritten");
+
+        }
+        try {
+            InputStream is = Files.newInputStream(in.toPath());
+            OutputStream os = Files.newOutputStream(out.toPath());
+            int sum = 0;
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1){
+                sum += len;
+                os.write(buffer,0,len);
+            }
+            System.out.println(sum);
+
+        }catch (IOException e){
+            System.out.println("Error");
+        }
+    }
+}
